d4ed688804313330014a043817cd23da0a977c40
xrfang <xrfang@gmail.com>
2021-04-19 17:53:02 +0800

    inintial implementation

--- main.go
+++ main.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"embed"
 	"flag"
 	"fmt"
+	"io"
+	"io/fs"
 	"net"
 	"net/http"
 	"os"
 	"path/filepath"
 )
+
+//go:embed resources/*
+var res embed.FS
 
 func main() {
 	ver := flag.Bool("version", false, "show version info")
@@ -26,13 +32,43 @@ func main() {
 		fmt.Println("ERROR: markdown file not provided")
 		os.Exit(1)
 	}
+	fn := flag.Arg(0)
+	dir := filepath.Dir(fn)
+	root, _ := fs.Sub(res, "resources")
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("HELLO WORLD"))
+		defer func() {
+			if e := recover(); e != nil {
+				http.Error(w, trace("%v", e).Error(), http.StatusInternalServerError)
+			}
+		}()
+		f, err := root.Open(filepath.Join(".", r.URL.Path))
+		if err == nil {
+			defer f.Close()
+			_, err = io.Copy(w, f)
+			assert(err)
+			return
+		}
+		if !os.IsNotExist(err) {
+			panic(err)
+		}
+		f, err = os.Open(filepath.Join(dir, r.URL.Path))
+		if err != nil {
+			if os.IsNotExist(err) {
+				http.Error(w, fmt.Sprintf("not found: %s", r.URL.Path), http.StatusNotFound)
+				return
+			}
+			panic(err)
+		}
+		defer f.Close()
+		_, err = io.Copy(w, f)
+		assert(err)
+	})
+	http.HandleFunc("/render", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(RenderMD(fn)))
 	})
 	ln, err := net.Listen("tcp", ":0")
 	assert(err)
 	port := ln.Addr().(*net.TCPAddr).Port
-	fmt.Println("Using port:", port)
-	go open(fmt.Sprintf("http://127.0.0.1:%d/", port))
+	go open(fmt.Sprintf("http://127.0.0.1:%d/index.html", port))
 	panic(http.Serve(ln, nil))
 }
